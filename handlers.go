b11633179c570b56ede0a6cfd662746506f15c59
vvaselin <0pw3292219w259d@gmail.com>
2026-05-21 18:02:56 +0900

    emotion出力

--- handlers.go
+++ handlers.go
@@ -208,7 +208,7 @@ func chatWSHandler(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		log.Printf("[Stream] params=%+v", chatRes.Parameters)
+		log.Printf("[Stream] params=%+v emo=%s", chatRes.Parameters, chatRes.Emotion)
 
 		// 会話履歴に今回のやり取りを追加
 		chatHistory = append(chatHistory, OpenAIMessage{
